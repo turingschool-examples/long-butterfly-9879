127c30695e99888e3150c1d74be4213bda4dc7a8
Harrison Ryan <hwryan12@gmail.com>
2023-02-26 09:19:34 -0600

    feature: Set up db and and migrations for mechanics table and relations

---
+++ db/migrate/20230226150642_create_mechanics.rb
@@ -0,0 +1,10 @@
+class CreateMechanics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :mechanics do |t|
+      t.string :name
+      t.integer :years_experience
+      t.references :ride, foreign_key: true
+      t.timestamps
+    end
+  end
+end
