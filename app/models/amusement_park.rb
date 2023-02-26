37a0bbb07dc16d1685771c4c95ec2dbed5588cd9
KaraJH <karas.jones@yahoo.com>
2023-02-26 16:22:27 -0700

    feat: US 3 Complete. Amusement park show page

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,9 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+  has_many :mechanic_rides, through: :rides 
+  has_many :mechanics, through: :mechanic_rides
+
+  def mechanics_at_park 
+    mechanics.distinct
+  end
 end
