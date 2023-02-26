46ff3c89f990b3c21ad6db783919ea1435afeff4
Matt Enyeart <menyeart1@gmail.com>
2023-02-26 12:41:49 -0700

    Add ride and avg exp instance variable

--- app/controllers/amusement_parks_controller.rb
+++ app/controllers/amusement_parks_controller.rb
@@ -3,5 +3,6 @@ class AmusementParksController < ActionController::Base
   def show
     @amusement_park = AmusementPark.find(params[:id])
     @mechanics = @amusement_park.list_mechanics_uniq
+    @rides_and_avg_exp = @amusement_park.list_rides_and_mech_exp
   end
 end
