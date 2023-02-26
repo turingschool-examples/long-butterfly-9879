cc56422a9308278965136bcba3094270ac62cc8a
Matt Enyeart <menyeart1@gmail.com>
2023-02-26 12:46:09 -0700

    Remove extra spacing

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe Mechanic, type: :model do
   describe 'relationships' do
     it {should have_many :ride_mechanics}
     it {should have_many(:rides).through(:ride_mechanics)}
-    
   end
 end
