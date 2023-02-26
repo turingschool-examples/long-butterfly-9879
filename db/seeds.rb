a92e9789a61543ddcfa67b8d8e445bfe63b9deab
Matt Enyeart <menyeart1@gmail.com>
2023-02-26 12:44:35 -0700

    Create additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AmusementPark.destroy_all
 Ride.destroy_all
+Mechanic.destroy_all
+RideMechanic.destroy_all
 
 @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
 @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
@@ -16,3 +18,14 @@ Ride.destroy_all
 @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
 @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+@mechanic_1 = Mechanic.create!(name: 'Stephanie Smith', years_experience: 2)
+@mechanic_2 = Mechanic.create!(name: 'Matt Smith', years_experience: 6)
+
+@ride_mechanic1 = RideMechanic.create!(mechanic: @mechanic_1, ride: @hurler)
+@ride_mechanic2 = RideMechanic.create!(mechanic: @mechanic_1, ride: @scrambler)
+@ride_mechanic3 = RideMechanic.create!(mechanic: @mechanic_1, ride: @ferris)
+
+@ride_mechanic1 = RideMechanic.create!(mechanic: @mechanic_2, ride: @hurler)
+@ride_mechanic2 = RideMechanic.create!(mechanic: @mechanic_2, ride: @scrambler)
+
