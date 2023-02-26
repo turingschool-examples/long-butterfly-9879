7cc43482ed77ae33ad3da9565d56d4162b679538
Matt Enyeart <menyeart1@gmail.com>
2023-02-26 11:55:24 -0700

    Refactor tests for mechanics feature show spec

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe 'the mechanic show page' do
         expect(page).to have_content("#{coaster.name}")
         expect(page).to have_content("#{wheel.name}")
         expect(page).to_not have_content("#{slide.name}")
-
       end
 
       it "I see a form to add a ride to their workload, when I fill in that field with an id of an existing ride and click submit, I'm taken back to that mechanics show page and I see the name of the newly added ride" do
@@ -43,7 +42,7 @@ RSpec.describe 'the mechanic show page' do
 
         fill_in("Ride ID", with: slide.id)
         click_button("Submit")
-     
+        
         expect(current_path).to eq("/mechanics/#{mechanic_1.id}")
         expect(page).to have_content("#{slide.name}")
       end
