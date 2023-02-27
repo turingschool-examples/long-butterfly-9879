f923b8630f5f5fdf1fb8dc1024bcfdeb6079399a
Harrison Ryan <hwryan12@gmail.com>
2023-02-27 09:06:01 -0600

    feature: Create test for user story 2

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -10,16 +10,18 @@ RSpec.describe "Mechanics#Show", type: :feature do
     @hurler = @flags.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
     @drop = @flags.rides.create!(name: "The Dungeon Drop", thrill_rating: 8, open: true)
     @splash = @flags.rides.create!(name: "Splash Mountain", thrill_rating: 6, open: true)
+    @carousel = @flags.rides.create!(name: "Carousel", thrill_rating: 1, open: true)
 
     MechanicRide.create!(mechanic_id: @kara.id, ride_id: @hurler.id)
     MechanicRide.create!(mechanic_id: @kara.id, ride_id: @drop.id)
     MechanicRide.create!(mechanic_id: @amos.id, ride_id: @splash.id)
+    
+    visit "/mechanics/#{@kara.id}"
   end
 
   describe "User Story 1" do
     context "As a user, when I visit a mechanic show page" do
       it "I see their name, years of experience, and the names of all rides they are working on" do
-        visit "/mechanics/#{@kara.id}"
         
         within("#mechanic_info") do
           expect(page).to have_content("Name: Kara Smith")
@@ -29,6 +31,24 @@ RSpec.describe "Mechanics#Show", type: :feature do
           expect(page).to_not have_content("Name: Amos Burton")
           expect(page).to_not have_content("Years of Experience: 20")
           expect(page).to_not have_content("Splash Mountain")
+        end
+      end
+    end
+  end
+
+  describe "User Story 2" do
+    context "As a user, when I visit a mechanic show page" do
+      it "I see a form to add a ride to their workload when I fill in that field with an 
+        id of an existing ride and click Submit I'm taken back to that mechanic's show page 
+        and I see the name of that newly added ride on this mechanic's show page" do
+     
+        fill_in :ride_id, with: @carousel.id
+        click_button "Submit"
+
+        expect(current_path).to eq("/mechanics/#{@kara.id}")
+        
+        within("#mechanic_info") do
+          expect(page).to have_content("Carousel")
         end
       end
     end
