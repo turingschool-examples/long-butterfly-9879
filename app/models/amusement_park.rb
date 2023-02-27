61fa1a45e64f1d2b133b5d9ddb86c08d11e29d20
KaraJH <karas.jones@yahoo.com>
2023-02-26 17:28:06 -0700

    Complete extension, Orders by average years of experience in amusement park show page

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -6,4 +6,8 @@ class AmusementPark < ApplicationRecord
   def mechanics_at_park 
     mechanics.distinct
   end
+
+  def order_rides_by_mechanic_experience 
+    rides.joins(:mechanics).select('rides.*, avg(mechanics.years_experience) as average').group(:id).order('average desc')
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :mechanic_rides 
   has_many :mechanics, through: :mechanic_rides
+
+  def avg_years_experience 
+    mechanics.average(:years_experience)
+  end
 end
