592a8932e2b6937ce161adf7b2283816f68d97e0
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-27 15:17:45 -0700

    Add all relationship tests

--- spec/models/ride_mechanic_spec.rb
+++ spec/models/ride_mechanic_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe RideMechanic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :ride }
+  it { should belong_to :mechanic }
 end
