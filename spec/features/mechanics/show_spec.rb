bf84c4ce104f3b100b056be6c3f2a797c2dfcfd5
Harrison Ryan <hwryan12@gmail.com>
2023-02-27 08:51:55 -0600

    feature: complete user story 1

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Mechanics#Show", type: :feature do
-  before do
+  before :each do
     @flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
     
     @kara = Mechanic.create!(name: "Kara Smith", years_experience: 11)
     @amos = Mechanic.create!(name: "Amos Burton", years_experience: 20)
 
-    @hurler = @kara.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
-    @drop = @kara.rides.create!(name: "The Dungeon Drop", thrill_rating: 8, open: true)
-    @hurler = @amos.rides.create!(name: "Splash Mountain", thrill_rating: 6, open: true)
+    @hurler = @flags.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
+    @drop = @flags.rides.create!(name: "The Dungeon Drop", thrill_rating: 8, open: true)
+    @splash = @flags.rides.create!(name: "Splash Mountain", thrill_rating: 6, open: true)
+
+    MechanicRide.create!(mechanic_id: @kara.id, ride_id: @hurler.id)
+    MechanicRide.create!(mechanic_id: @kara.id, ride_id: @drop.id)
+    MechanicRide.create!(mechanic_id: @amos.id, ride_id: @splash.id)
   end
 
   describe "User Story 1" do
@@ -17,12 +21,15 @@ RSpec.describe "Mechanics#Show", type: :feature do
       it "I see their name, years of experience, and the names of all rides they are working on" do
         visit "/mechanics/#{@kara.id}"
         
-        expect(page).to have_content("Name: Kara Smith")
-        expect(page).to have_content("Years of Experience: 11")
-        expect(page).to have_content("Rides Currently Working on: The Hurler, The Dungeon Drop")
-        expect(page).to_not have_content("Name: Amos Burton")
-        expect(page).to_not have_content("Years of Experience: 20")
-        expect(page).to_not have_content("Rides Currently Working on: Splash Mountain")
+        within("#mechanic_info") do
+          expect(page).to have_content("Name: Kara Smith")
+          expect(page).to have_content("Years of Experience: 11")
+          expect(page).to have_content("The Hurler The Dungeon Drop")
+          expect(page).to have_content("The Dungeon Drop")
+          expect(page).to_not have_content("Name: Amos Burton")
+          expect(page).to_not have_content("Years of Experience: 20")
+          expect(page).to_not have_content("Splash Mountain")
+        end
       end
     end
   end
