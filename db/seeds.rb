d062ae00d8d3e5f856297f8d138f07a5df18f3de
Caleb Thomas <cjthomas00@comcast.net>
2023-02-26 18:14:19 -0700

    add mechanics, rides, park to see data for server.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,11 @@ Ride.destroy_all
 @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
 @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+@park = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+@ride1 = @park.rides.create!(name: "The Hurler", thrill_rating: 7, open: false)
+@ride2 = @park.rides.create!(name: "Tower of Doom", thrill_rating: 10, open: true)
+@ride3 = @park.rides.create!(name: "Tea Cups", thrill_rating: 3, open: true)
+@mechanic1 = Mechanic.create!(name: "Kara Smith", years_experience: 11)
+@mechanic2 = Mechanic.create!(name: "Karl Smits", years_experience: 11)
+MechanicRide.create!(mechanic_id: @mechanic1.id, ride_id: @ride1.id)
+MechanicRide.create!(mechanic_id: @mechanic1.id, ride_id: @ride2.id)
