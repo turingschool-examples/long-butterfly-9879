b1ae16f354fb704c8227c45f0e8979c20281fd7d
Caleb Thomas <cjthomas00@comcast.net>
2023-02-26 17:37:07 -0700

    create the rest of US1 test.

--- spec/features/mechanic/show_spec.rb
+++ spec/features/mechanic/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Show", type: :feature do
       @ride2 = @park.rides.create!(name: "Tower of Doom", thrill_rating: 10, open: true)
       @ride3 = @park.rides.create!(name: "Tea Cups", thrill_rating: 3, open: true)
       @mechanic1 = Mechanic.create!(name: "Kara Smith", years_experience: 11)
+      @mechanic2 = Mechanic.create!(name: "Karl Smits", years_experience: 11)
       MechanicRide.create!(mechanic_id: @mechanic1.id, ride_id: @ride1.id)
       MechanicRide.create!(mechanic_id: @mechanic1.id, ride_id: @ride2.id)
       visit mechanic_path(@mechanic1)
@@ -15,6 +16,13 @@ RSpec.describe "Show", type: :feature do
     describe "When I visit the mechanic show page" do
       it "I see their name, years of experience, and the names of all rides they are working on" do
         expect(current_path).to eq(mechanic_path(@mechanic1))
+        expect(page).to have_content(@mechanic1.name)
+        expect(page).to have_content("Years of Experience: #{@mechanic1.years_experience}")
+        expect(page).to have_content("Rides Currently Working On:")
+        expect(page).to have_content(@ride1.name)
+        expect(page).to have_content(@ride2.name)
+        expect(page).to have_no_content(@ride3.name)
+        expect(page).to have_no_content(@mechanic2.name)
       end
     end
   end
