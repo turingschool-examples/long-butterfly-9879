d6dca1fe94ff770f293c15669ae6cef484fc86f9
Weston Sandfort <sandfortw@gmail.com>
2023-02-27 20:08:07 -0700

    create amusement parks show spec skeleton

---
+++ spec/features/amusement_parks/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'amusement park show page' do
+
+  describe 'amusement park details' do
+    it 'has its name'
+    it 'has its admission price'
+    it 'has a list of mechanics'
+    it 'that list is of unique mechanics'
+  end
+end
