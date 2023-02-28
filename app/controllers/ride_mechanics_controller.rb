d43ef4fd4174cfeb49070a328fed83b8d5b59425
Andra Helton <andrahelton@gmail.com>
2023-02-27 21:19:00 -0700

    Add method to appropriate controller

---
+++ app/controllers/ride_mechanics_controller.rb
@@ -0,0 +1,6 @@
+class RideMechanicsController < ApplicationController
+  def create
+    RideMechanic.create!(ride_id: params[:ride_id], mechanic_id: params[:id])
+    redirect_to "/mechanics/#{params[:id]}"
+  end
+end
