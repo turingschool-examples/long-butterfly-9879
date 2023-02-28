98b5c6702cf3cffeec47b4c9648868d7ec217de7
Adam Bailey <ab67319@gmail.com>
2023-02-27 17:42:31 -0700

    feat: create model associations and validations

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,6 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  validates_presence_of :name, :admission_cost
+  validates_numericality_of :admission_cost
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,7 @@
 class Mechanic < ApplicationRecord
-  
+  validates_presence_of :name, :years_experience
+  validates_numericality_of :years_experience
+
+  has_many :ride_mechanics
+  has_many :rides, through: :ride_mechanics
 end
