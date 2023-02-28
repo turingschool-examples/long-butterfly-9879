8acb12c0c2e6cad4041dba324b714a4ed6c08ae6
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-27 21:12:26 -0700

    test: Add functionality for user story 2

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -12,14 +12,37 @@ RSpec.describe "The Mechanic's Show Page" do
 
     @ride_mechanic_1 = RideMechanic.create!(ride_id: @hurler.id, mechanic_id: @mechanic.id)
     @ride_mechanic_2 = RideMechanic.create!(ride_id: @scrambler.id, mechanic_id: @mechanic.id)
-    @ride_mechanic_3 = RideMechanic.create!(ride_id: @ferris.id, mechanic_id: @mechanic.id)
 
     visit "/mechanics/#{@mechanic.id}"
   end
     describe "User Story 1" do 
-      it "When I visit a mechanic show page I see their name, years of experience, and the names of all rides they are working on" do
-        expect(page).to have_content("Mechanic's Name: Dawson")
+      it "shows the visitor the mechanic's name, years of experience, and the names of all rides they are working on" do
+        expect(page).to have_content("Mechanic: Dawson")
         expect(page).to have_content("Years of experience: 5")
+
+        within("#mechanic-rides") {
+          expect(page).to have_content('The Hurler')
+          expect(page).to have_content('The Scrambler')
+        }
+      end
+    end
+
+    describe "User Story 2" do
+      it "shows the visitor a form to add a ride to their workload" do 
+        within("#new-ride-form") {
+          expect(page).to have_content("Ride Id:")
+          expect(page).to have_field("ride_id")
+          expect(page).to have_button("Submit")
+        }
+      end
+
+      it "allows the visitor to fill in that field with an id of an existing ride and click Submit. 
+      They are then taken back to that mechanic's show page, and see the name of that newly added ride on the mechanic's show page" do
+        fill_in "ride_id", with: @ferris.id
+
+        click_on "Submit"
+
+        expect(current_path).to eq(mechanic_path(@mechanic))
 
         within("#mechanic-rides") {
           expect(page).to have_content('The Hurler')
