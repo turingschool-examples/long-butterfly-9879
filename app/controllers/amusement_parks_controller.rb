91f5f5367f18faf506a15bb13a014fe56e1376bd
Chris Crane <chrisjcrane@gmail.com>
2023-02-27 19:47:08 -0700

    pass first test which displays park ride names

--- app/controllers/amusement_parks_controller.rb
+++ app/controllers/amusement_parks_controller.rb
@@ -2,5 +2,6 @@ class AmusementParksController < ApplicationController
   def show
     @amusement_park = AmusementPark.find(params[:id])
     @mechanics = @amusement_park.mechanics.distinct
+    @rides = @amusement_park.rides.distinct
   end
 end
