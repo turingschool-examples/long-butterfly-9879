210e892621ec3a76725076016d4f1f623d1f3258
Adam Bailey <ab67319@gmail.com>
2023-02-27 18:56:24 -0700

    feat: create story 2 feature, add a ride to a mechanic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :mechanics, only: :show
+  
+  resources :mechanics, only: :show do
+    resources :rides, only: :create, controller: "ride_mechanics"
+  end
 end
