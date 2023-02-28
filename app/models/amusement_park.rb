f1479e7b06545b89803d0825582cb572a54792e0
Adam Bailey <ab67319@gmail.com>
2023-02-27 19:34:47 -0700

    feat: create model method for #unique_mechanics

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -3,4 +3,8 @@ class AmusementPark < ApplicationRecord
 
   validates_presence_of :name, :admission_cost
   validates_numericality_of :admission_cost
+
+  def unique_mechanics
+    Mechanic.joins(rides: :amusement_park).where("amusement_parks.id = ?", self.id).distinct
+  end
 end
