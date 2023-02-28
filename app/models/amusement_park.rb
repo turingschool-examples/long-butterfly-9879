e6cc8e092889c37462b10263da5e5150a851172c
Weston Sandfort <sandfortw@gmail.com>
2023-02-27 20:31:52 -0700

    create unique mechanics names method and add mechanics relationship to amusement park

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,7 +1,12 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+  has_many :ride_mechanics, through: :rides 
+  has_many :mechanics, through: :ride_mechanics
   validates_presence_of :name
   validates_presence_of :admission_cost
   validates_numericality_of :admission_cost
   
+  def unique_mechanics_names
+    mechanics.distinct.pluck(:name)
+  end
 end
