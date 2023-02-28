2e18303bf2f8531348177e0cdeb1cbe186847986
Adam Bailey <ab67319@gmail.com>
2023-02-27 20:14:53 -0700

    test: create model test for #mechanics_average_experience_by_ride

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -12,16 +12,16 @@ RSpec.describe AmusementPark, type: :model do
   end
 
   describe '#Instance Methods' do
-    describe '#unique_mechanics' do
-      before(:each) do
-        @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-      
-        @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-        @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-        @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
-      
-      end
+    before(:each) do
+      @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    
+      @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    
+    end
 
+    describe '#unique_mechanics' do
       it 'returns a list of all mechanics working on rides for amusement park' do
         jim = Mechanic.create!(name: "Jim", years_experience: 10)
         bob = Mechanic.create!(name: "Bob", years_experience: 15)
@@ -47,6 +47,30 @@ RSpec.describe AmusementPark, type: :model do
         RideMechanic.create!(ride: @scrambler, mechanic: bob)
 
         expect(@six_flags.unique_mechanics).to eq([jim, bob])
+      end
+    end
+
+    describe '#mechanics_average_experience_by_ride' do
+      it 'returns a list of rides associated with the park and average experience of mechanics working on the ride' do
+        adam = Mechanic.create!(name: "Adam", years_experience: 0)
+        jim = Mechanic.create!(name: "Jim", years_experience: 10)
+        bob = Mechanic.create!(name: "Bob", years_experience: 15)
+        
+        RideMechanic.create!(ride: @scrambler, mechanic: bob)
+        RideMechanic.create!(ride: @scrambler, mechanic: adam)
+        
+        RideMechanic.create!(ride: @hurler, mechanic: jim)
+        RideMechanic.create!(ride: @hurler, mechanic: bob)
+        
+        RideMechanic.create!(ride: @ferris, mechanic: jim)
+        RideMechanic.create!(ride: @ferris, mechanic: adam)
+
+        query_result = @six_flags.mechanics_average_experience_by_ride
+
+        expect(query_result).to eq([@hurler, @scrambler, @ferris])
+        expect(query_result[0].average_experience).to eq(12.5)
+        expect(query_result[1].average_experience).to eq(7.5)
+        expect(query_result[2].average_experience).to eq(5)
       end
     end
   end
