216cc008aa91d923f754a24dd2f2dc7d11423555
Weston Sandfort <sandfortw@gmail.com>
2023-02-27 20:32:21 -0700

    complete user story 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :mechanics, only: [:show]
   resources :ride_mechanics, only: [:create]
-  resources :amusment_parks, only: [:show]
+  resources :amusement_parks, only: [:show]
 end
