78542b2bc4c540e6207e185a36397a34b361ed3f
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-28 14:34:09 -0700

    adds association for amusement parks and mechanics and mechanic_rides

--- app/controllers/amusement_parks_controller.rb
+++ app/controllers/amusement_parks_controller.rb
@@ -2,6 +2,5 @@ class AmusementParksController < ApplicationController
   def show
     @amusement_park = AmusementPark.find(params[:id])
     @amusement_park_mechanics = @amusement_park.list_of_mechanics
-    binding.pry
   end
 end
