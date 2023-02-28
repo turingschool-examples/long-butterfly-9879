f6609bde3d67292a4c7c858b60a3d2770747ced9
Bobby Luly <bobbyluly@gmail.com>
2023-02-28 15:18:17 -0700

    Create all models, tests, migrations, and associations

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many(:rides) }
+    it { should have_many(:ride_mechanics).through(:rides) }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
+
   end
 end
