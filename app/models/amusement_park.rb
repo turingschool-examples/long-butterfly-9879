c525738219eb1693d1f96e35af473bf947ddbfda
Adam Bailey <ab67319@gmail.com>
2023-02-27 20:15:08 -0700

    feat: create model method #mechanics_average_experience_by_ride

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -5,6 +5,16 @@ class AmusementPark < ApplicationRecord
   validates_numericality_of :admission_cost
 
   def unique_mechanics
-    Mechanic.joins(rides: :amusement_park).where("amusement_parks.id = ?", self.id).distinct
+    Mechanic.joins(rides: :amusement_park)
+    .where("amusement_parks.id = ?", self.id)
+    .distinct
+  end
+
+  def mechanics_average_experience_by_ride
+    self.rides
+    .joins(:mechanics)
+    .select("rides.*, avg(mechanics.years_experience) as average_experience")
+    .group(:id)
+    .order("average_experience desc")
   end
 end
