4a66578b2a6ba6d6f296422c995ac0120d9b2f93
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-28 14:22:14 -0700

    Feat: add functionality to fill out a form and add a ride to a mechanic

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,4 +5,8 @@ class MechanicsController < ApplicationController
     @rides = @mechanic.rides
   end
 
+  def create
+    ride_mechanic = RideMechanic.create!(mechanic_id: params[:id], ride_id: params[:ride_id])
+    redirect_to "/mechanics/#{params[:id]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  post '/mechanics/:id/rides', to: 'mechanics#create'
   get '/mechanics/:id', to: 'mechanics#show'
 end
