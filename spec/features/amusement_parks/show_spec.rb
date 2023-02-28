829139b0c900adef543def7ee0848696982cfc46
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-28 14:01:17 -0700

    start of user story three test setup

---
+++ spec/features/amusement_parks/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'amusement parks show page' do
+  let!(:amusement_park1) {AmusementPark.create!(name: "Six Flags", admission_cost: 75)}
+  let!(:amusement_park2) {AmusementPark.create!(name: "Carowinds", admission_cost: 65)}
+  
+  let!(:ride1) {Ride.create!(name: "The Hurler", thrill_rating: 7, open: false, amusement_park_id: amusement_park1.id )}
+  let!(:ride2) {Ride.create!(name: "Tiny Teacups", thrill_rating: 5, open: false, amusement_park_id: amusement_park1.id )}
+  let!(:ride3) {Ride.create!(name: "Big Slide", thrill_rating: 7, open: false, amusement_park_id: amusement_park2.id )}
+  let!(:ride4) {Ride.create!(name: "Danger Drop", thrill_rating: 9, open: false, amusement_park_id: amusement_park1.id )}
+
+  let!(:mechanic1) {Mechanic.create!(name: "Kara Smith", years_experience: 11)}
+  let!(:mechanic2) {Mechanic.create!(name: "Michael Scott", years_experience: 1)}
+
+
+  before do
+    MechanicRide.create!(ride: ride1, mechanic: mechanic1)
+    MechanicRide.create!(ride: ride2, mechanic: mechanic1)
+    MechanicRide.create!(ride: ride4, mechanic: mechanic2)
+    MechanicRide.create!(ride: ride2, mechanic: mechanic2)
+  end
+
+  describe 'as a visitor to the amusement parks show page' do
+    it 'I see the name and price of admissions for that park, and see the names of all the mechanics that are working on that parks rides (unique list)' do
+
+    end
+  end
+end
