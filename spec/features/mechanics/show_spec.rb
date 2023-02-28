ef59e2569f3ae131ac6acbe4bc68453a58ff4219
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-28 14:13:08 -0700

    Test: add test for filling out a form and adding a new ride to a mechanic

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -37,6 +37,27 @@ RSpec.describe 'Mechanic show page' do
         expect(page).to have_button("Submit")
         }
       end
+
+      it 'can fill the form out and add a new ride to a mechanic' do 
+        park = AmusementPark.create!(name: "Six Flags", admission_cost: 10)
+        ride1 = park.rides.create!(name: "Tower of doom", thrill_rating: 10, open: true)
+        ride2 = park.rides.create!(name: "Splash Mountain", thrill_rating: 10, open: true)
+        ride3 = park.rides.create!(name: "Twister", thrill_rating: 10, open: true)
+        mechanic = Mechanic.create!(name: "Brian", years_experience: 0)
+        RideMechanic.create!(mechanic: mechanic, ride: ride1)
+        RideMechanic.create!(mechanic: mechanic, ride: ride2)
+
+        visit "/mechanics/#{mechanic.id}"
+
+        expect(page).to have_no_content(ride3.name)
+        
+        fill_in :ride_id, with: ride3.id
+        click_button 'Submit'
+        
+        expect(page).to have_current_path("/mechanics/#{mechanic.id}")
+
+        expect(page).to have_content(ride3.name)
+      end
     end
   end
 end
