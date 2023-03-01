bd8d6c5deb6d70c7ea2e78dead47ee521500bd2b
Bobby Luly <bobbyluly@gmail.com>
2023-02-28 17:30:30 -0700

    Feat: create #mechanic_names instance method

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -2,4 +2,8 @@ class AmusementPark < ApplicationRecord
   has_many :rides
   has_many :ride_mechanics, through: :rides
   has_many :mechanics, through: :ride_mechanics
+
+  def mechanic_names
+    mechanics.distinct.pluck(:name)
+  end
 end
