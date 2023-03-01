d8941c5dbed8f47fb777f3490e73f45e5fea41d9
Jade Stewart <jadekstewart3@gmail.com>
2023-02-28 17:21:37 -0700

    Create post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :mechanics, only: :show
-
+  # resources :mechanics, only: :show
+  get '/mechanics/:id', to: 'mechanics#show'
   post 'mechanics/:id', to: 'mechanic_rides#create'
 end
