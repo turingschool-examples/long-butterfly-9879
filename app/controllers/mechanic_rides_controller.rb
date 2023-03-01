1591019c03dc3353765faaf7aefbd3d1dc2a457d
Jade Stewart <jadekstewart3@gmail.com>
2023-02-28 17:21:22 -0700

    Adjust create method for controller

--- app/controllers/mechanic_rides_controller.rb
+++ app/controllers/mechanic_rides_controller.rb
@@ -1,18 +1,12 @@
 class MechanicRidesController < ApplicationController
   def create
-    mechanic = Mechanic.find(params[:id])
-    ride = Ride.find(params[:ride_id])
-    mechanic_ride = MechanicRide.create!(mechanic_id: params[:id], ride_id: params[:ride_id])
-    redirect_to mechanic_path(mechanic)
+    mechanic_ride = MechanicRide.new(mechanic_id: params[:id], ride_id: params[:ride_id])
+    
+    redirect_to "/mechanics/#{mechanic_ride.mechanic_id}"
     if mechanic_ride.save!
       flash[:info] = "Added Ride"
     else
       flash[:error] = "Error: #{error_message(mechanic_ride.errors)}"
     end
-  end
-
-  private 
-  def mechanic_rides_params
-    params.permit(:ride_id, :mechanic_id)
   end
 end
