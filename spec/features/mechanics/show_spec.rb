d01e7ea2ed67e4fd72afab9dbd63f137a310f652
James Taylor <jrtaylor0614@gmail.com>
2023-02-28 17:05:14 -0700

    Adds test for US2/ Add a Ride to a Mechanic form

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe "Mechantic show page" do
     @ride_2 = @amusement_park.rides.create!(name: "ride_2", thrill_rating: 3, open: true)
     @ride_3 = @amusement_park.rides.create!(name: "ride_3", thrill_rating: 4, open: true)
     @ride_4 = @amusement_park.rides.create!(name: "ride_4", thrill_rating: 5, open: false)
+    @ride_5 = @amusement_park.rides.create!(name: "ride_5", thrill_rating: 6, open: false)
     @mechanic_1 = Mechanic.create!(name: "Guy the mechanic", years_experience: 22)
     @mechanic_2 = Mechanic.create!(name: "Girl the mechanic", years_experience: 33)
     @mechanic_ride_1 = MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_1.id)
     @mechanic_ride_2 = MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_2.id)
     @mechanic_ride_3 = MechanicRide.create!(mechanic_id: @mechanic_2.id, ride_id: @ride_3.id)
     @mechanic_ride_4 = MechanicRide.create!(mechanic_id: @mechanic_2.id, ride_id: @ride_4.id)
+    @mechanic_ride_5 = MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_5.id)
   end
 
   describe "As a user" do
@@ -30,6 +32,31 @@ RSpec.describe "Mechantic show page" do
           expect(page).to have_content("ride_2")
           expect(page).to_not have_content("ride_3")
           expect(page).to_not have_content("ride_4")
+        end
+      end
+
+      describe "I see a form to add a ride to their workload" do
+        describe "When I fill in that field with an id of an existing ride and click Submit" do
+          describe "Im taken back to that mechanic's show page" do
+            it "And I see the name of that newly added ride on this mechanic's show page" do 
+              visit mechanic_path(@mechanic_1)
+
+              expect(page).to have_content("Add a ride to #{@mechanic_1.name} workflow")
+              
+              expect(page).to not_have_content("ride_5")
+
+              within "#add_ride_form" do
+                
+                fill_in "Ride ID", with: @ride_5.id
+                
+                click_button("Submit")
+                
+                expect(current_path).to eq(mechanic_path(@mechanic_1))
+              end
+              
+              expect(page).to have_content("ride_5")
+            end
+          end
         end
       end
     end
